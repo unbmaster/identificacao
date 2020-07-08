2f813c05f75da7ddac5f738c790c563501d00d02
UnBMaster <unbmaster@outlook.com>
2020-07-08 07:34:29 -0300

    Update 4

--- public/index.php
+++ public/index.php
@@ -20,9 +20,11 @@ use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
 
-$app->add(new Core\CorrelationIdMiddleware());
+$auth = new Core\IdentificacaoMiddleware;
 
-$app->any('/login', 'Controller\IdentificacaoController:login');
+$app->add(new Core\CorrelationIdMiddleware);
+
+$app->any('/identificacao/login', 'Controller\IdentificacaoController:login');
 
 $app->any('/identificacao', 'Controller\HomeController:api');
 
